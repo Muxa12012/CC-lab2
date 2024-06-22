733c708af572f52c1ee03f63d4a79841611acb22
Семёнов Михаил <msemenoff@mail.ru>
2024-06-22 09:38:48 +0300

    Update LabApplicationTests.java

--- lab/src/test/java/bmstu/lab/LabApplicationTests.java
+++ lab/src/test/java/bmstu/lab/LabApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-c
+@Slf4j
 @SpringBootTest
 @RunWith(value = Parameterized.class)
 class LabApplicationTests {
